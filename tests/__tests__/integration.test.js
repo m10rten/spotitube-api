401f8238d2aea07a0cb97fe82526aab0ea5f09cf
morty <mr.vanderlei@student.han.nl>
2022-03-30 23:04:03 +0200

    test + docs

--- tests/__tests__/integration.test.js
+++ tests/__tests__/integration.test.js
@@ -7,53 +7,85 @@ require('dotenv').config();
 // make request from supertest
 const request = supertest(process.env.API_URL || 'http://localhost:8010');
 
+// token to use for playlists: token gets updated in the test: /login correct
 let token = '';
-// write basic test function 
+
 describe('/login', () => {
   it('POST /login correct creds', async () => {
-    await request.post(`/login`)
-      .send({
-        user: 'morty',
-        password: 'mortypassword',
-      })
-      .expect(200)
-      .then((res) => {
-        expect(res.body.token).toBeDefined();
-        expect(res.body.user).toBeDefined();
-        token = res.body.token;
-      });
+    const times = [];
+    for(let i = 0; i < 10; i++) {
+      const start = Date.now();
+      await request.post(`/login`)
+        .send({
+          user: 'morty',
+          password: 'mortypassword',
+        })
+        .expect(200)
+        .then((res) => {
+          expect(res.body.token).toBeDefined();
+          expect(res.body.user).toBeDefined();
+          token = res.body.token;
+        });
+        const end = Date.now();
+        const time = end - start;
+        times.push(time);
+    } 
+    console.table(times);
   });
+
   it('POST /login wrong creds', async () => {
-    await request.post(`/login`)
-      .send({
-        user: 'morty',
-        password: 'userFalsePassword',
-      })
-      .expect(401)
-      .then((res) => {
-        expect(res.body.token).toBe(undefined);
-        expect(res.body.user).toBe(undefined);
-      });
+    const times = [];
+    for(let i = 0; i < 10; i++) {
+      const start = Date.now();
+      await request.post(`/login`)
+        .send({
+          user: 'user',
+          password: 'incorrect',
+        })
+        .expect(401)
+        .then((res) => {
+          expect(res.body.token).toBe(undefined);
+          expect(res.body.user).toBe(undefined);
+        });
+      const end = Date.now();
+      const time = end - start;
+      times.push(time);
+    }
+    console.table(times);
   });
 });
 
 // write a test to test the /playlists endpoint
 describe('/playlists', () => {
   it('GET /playlists correct token', async () => {
-    console.log(token);
-    await request.get(`/playlists?token=${token}`)
-      .expect(200)
-      .then((res) => {
-        console.log(res.body.playlists);
-        expect(res.body.playlists).toBeDefined();
-      });
+    const times = [];
+    for(let i = 0; i < 10; i++) {
+      const start = Date.now();
+      await request.get(`/playlists?token=${token}`)
+        .expect(200)
+        .then((res) => {
+          expect(res.body.playlists).toBeDefined();
+        });
+    const end = Date.now();
+    const time = end - start;
+    times.push(time);
+    }
+    console.table(times);
   });
-  // write a test to test the GET /playlists endpoint with an incorrect token
+
   it('GET /playlists incorrect token', async () => {
-    await request.get(`/playlists?token=wrongtoken`)
-      .expect(403)
-      .then((res) => {
-        expect(res.body.playlists).toBe(undefined);
-      });
+    const times = [];
+    const start = Date.now();
+    for(let i = 0; i < 10; i++) {
+      await request.get(`/playlists?token=wrongtoken`)
+        .expect(403)
+        .then((res) => {
+          expect(res.body.playlists).toBe(undefined);
+        });
+      const end = Date.now();
+      const time = end - start;
+      times.push(time);
+    }
+    console.table(times);
   });
 });
